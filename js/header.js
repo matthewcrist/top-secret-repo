cbd0e2f9a3f929dafc1b9aef08501c1bb0cdda6a
neogeek <neogeek0101@gmail.com>
2014-10-24 14:17:49 -0400

    Added header scroll / hide functionality.

--- js/header.js
+++ js/header.js
@@ -3,14 +3,47 @@ define(function (require) {
     'use strict';
 
     var $ = require('jquery'),
+        $window = $(window),
         $header = $('.site-header'),
-        $list = $header.find('nav ul');
+        $list = $header.find('nav ul'),
+        scroll = {
+            y: 0,
+            timeout: null
+        };
 
     $header.on('click', '.toggle-nav', function (e) {
 
         e.preventDefault();
 
         $list.toggle();
+
+    });
+
+    $window.on('scroll', function (e) {
+
+        clearTimeout(scroll.timeout);
+
+        if (screen.width < 960) {
+
+            scroll.timeout = setTimeout(function () {
+
+                var y = $window.scrollTop();
+
+                if (y < scroll.y || y < 200) {
+
+                    $header.stop(true, true).animate({ top: 0 });
+
+                } else {
+
+                    $header.stop(true, true).delay(500).animate({ top: -100 });
+
+                }
+
+                scroll.y = y;
+
+            }, 10);
+
+        }
 
     });
 
